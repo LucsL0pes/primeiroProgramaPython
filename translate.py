e0a70de0e459f103e3167b7bca09a01aa2c4b89f
LucsL0pes <112712042+LucsL0pes@users.noreply.github.com>
2023-05-23 03:49:15 -0300

    keys foram ocultas

--- translate.py
+++ translate.py
@@ -2,14 +2,13 @@ from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import unittest
 
-authenticator = IAMAuthenticator('Mz2537RTtqOWaQPTzs5OiRjpaZMdMVtCzOsBFSpluxpP')
+authenticator = IAMAuthenticator('key-api-ocult')
 language_translator = LanguageTranslatorV3(
     version='2018-05-01',
     authenticator=authenticator
 )
 
-language_translator.set_service_url('https://api.us-south.language-translator.watson.cloud.ibm.com/instances/c21cfa78-652b-4ed7-b6ff-0b3315ac7310')
-
+language_translator.set_service_url('Url-ocult')
 
 def english_to_french(text):
     translation = language_translator.translate(
